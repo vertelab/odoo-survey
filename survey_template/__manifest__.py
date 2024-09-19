f01e25045aa2f1897b7e78b2413bc8202a2bcb98
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-09-19 10:58:40 +0200

    Update __manifest__.py

--- survey_template/__manifest__.py
+++ survey_template/__manifest__.py
@@ -1,16 +1,23 @@
 {
-    "name": "Survey Template",
+    "name": "Survey: Template",
     "version": "17.0.1.0.0",
     'summary': 'Adds better template handling ',
-    'category': 'Survey',
     'description': """
 
     Define templates and join those with surveys
 
     """,
-    "author": "Vertel AB",
-    "license": "AGPL-3",
-    "website": "https://github.com/vertelab/odoo-survey",
+
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'author': 'Vertel AB',
+    'maintainer': 'Vertel AB',
+    'contributor': '',
+    'website': "https://vertel.se/apps/odoo-survey/survey_template",
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'repository': 'https://github.com/vertelab/odoo-survey',
+    # Any module necessary for this one to work correctly
+    
     "depends": ['survey',],
     'data': [
         'data/survey_data.xml',
