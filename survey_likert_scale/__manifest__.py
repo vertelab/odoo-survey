a2dc17d700869a67e2a7c84ee4d9bbe4cc18beab
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-09-19 10:00:22 +0200

    Update __manifest__.py

--- survey_likert_scale/__manifest__.py
+++ survey_likert_scale/__manifest__.py
@@ -1,14 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
-    "name": "Survey Likert Scale",
-    "version": "17.0.1.0.0",
+    "name": "Survey: Likert Scale",
+    "version": "1.0",
     'summary': 'Adds Likert Scale',
-    'category': 'Survey',
     'description': """
-Template for Likert Scale.
+    
+    Template for Likert Scale.
+    
     """,
-    "author": "Vertel AB",
-    "license": "AGPL-3",
-    "website": "https://github.com/vertelab/odoo-survey",
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'author': 'Vertel AB',
+    'maintainer': 'Vertel AB',
+    'contributor': '',
+    'website': "https://vertel.se/apps/odoo-survey/survey_likert_scale",
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'repository': 'https://github.com/vertelab/odoo-survey',
+    # Any module necessary for this one to work correctly
+
     "depends": ['survey'],
     'data': [
         'security/ir.model.access.csv',
