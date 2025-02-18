50eee7efa9af872857ceb94c7467bc634ba69115
marcuszxc <marcuszxc10@gamil.com>
2025-02-18 11:12:03 +0000

    odoobranchpfile survey_template from 17.0

---
+++ survey_template/__manifest__.p.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Survey: Template',
+    # #if VERSION == "18.0"
+    'version': '18.0.1.0.0',
+    # #elif VERSION == "17.0" 
+    'version': '17.0.1.0.0',
+    # #else
+    version': '1.0',
+    # #endif
+    'summary': 'Adds better template handling ',
+    'description': """
+
+    Define templates and join those with surveys
+
+    """,
+
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'author': 'Vertel AB',
+    'maintainer': 'Vertel AB',
+    'contributor': '',
+    'website': "https://vertel.se/apps/odoo-survey/survey_template",
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'repository': 'https://github.com/vertelab/odoo-survey',
+    # Any module necessary for this one to work correctly
+    
+    "depends": ['survey',],
+    'data': [
+        'data/survey_data.xml',
+        'views/survey_view.xml',
+       ],
+    "installable": True,
+    "auto_install": False,
+}
