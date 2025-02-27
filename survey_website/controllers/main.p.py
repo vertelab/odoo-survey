cac696d1039c7df8bfb8ef3338ed38e71a144222
iammiracle <miracleaayodele@gmail.com>
2025-02-27 17:00:44 +0100

    dynamically show things from the description field on survey and questions

--- survey_website/controllers/main.p.py
+++ survey_website/controllers/main.p.py
@@ -2,6 +2,7 @@ import logging
 from markupsafe import Markup
 from jinja2 import Template
 from odoo.addons.survey.controllers.main import Survey
+from odoo.http import request, content_disposition
 
 _logger = logging.getLogger(__name__)
 
@@ -10,9 +11,26 @@ class SurveyExtended(Survey):
 
     def _prepare_survey_data(self, survey_sudo, answer_sudo, **post):
         data = super()._prepare_survey_data(survey_sudo, answer_sudo, **post)
-        template = Template(survey_sudo.description)
+
+        survey = data.get('survey')
+        answer = data.get('answer')
+        question = data.get('question')
+
+        survey_description = Template(survey_sudo.description)
+        question_description = "<div></div>"
+
+        if question and question.description:
+            question_description = Template(question.description)
+
         context = {
-            "object": survey_sudo,  # Pass survey as "object"
+            # "object": data,
+
+            "answer": answer,
+            "survey": survey,
+            "user_id": request.env.user,
         }
-        data['description'] = Markup(template.render(context))
+        data['survey_description'] = Markup(survey_description.render(context))
+        if question and question.description:
+            data['question_description'] = Markup(question_description.render(context))
+
         return data
